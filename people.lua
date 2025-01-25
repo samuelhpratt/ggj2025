0a9ec091b6c410eb324e61beaf70a45d4cdd77af
shiny_shoes1 <mmurp30@hotmail.com>
2025-01-25 17:04:44 +1100

    weighted move for food working

--- people.lua
+++ people.lua
@@ -1,10 +1,10 @@
-nPeople = 100
-peopleSpeed = 1
+nPeople = 10
+peopleSpeed = 5
 
 function spawnPerson(x, y, z)
     local person = spawnObject(x, y, z)
     person.sprite = flr(rnd(2)) + 2
-    person.color = flr(rnd(16))
+    person.color = flr(rnd({8,10,12}))
     -- add any other person-specific parameters here!
 
     function person.draw(self)
@@ -17,7 +17,8 @@ function spawnPerson(x, y, z)
 
     function person.update(self)
         -- add person-specific update logic here
-        randomMove(self)
+        --randomMove(self)
+        weightedMove(self)
 
         self:updatePhysics()
     end
@@ -27,19 +28,55 @@ for i = 1, nPeople do
     spawnPerson(rnd(80) - 40, rnd(80) - 40, 0)
 end
 
-function updatePeople()
-    for i = 1, #objects do
-        if objects[i].type == "person" then
-            randomMove(objects[i])
-        else
-            --do nothing
+--helpers
+
+
+
+function distanceToObject(objectA, objectB)
+    local dx = objectA.x - objectB.x
+    local dy = objectA.y - objectB.y
+    return sqrt(dx ^ 2 + dy ^ 2)
+end
+
+
+
+function weightedMove(person)
+
+    local closestFood = {x=999, y=999}
+    local shortestDistance = 30000
+    local foodExists = false
+    local dist = 0
+    
+    local foodForcePower = 0.01
+    
+    
+
+    for object in all(objects) do
+        if object.sprite == objectSprites.food then
+            foodExists = true
+            dist = distanceToObject(person, object)
+            if dist < shortestDistance then
+                closestFood = object
+                shortestDistance = dist
+            end
         end
     end
+
+    log(shortestDistance)
+
+    if foodExists then
+        local finalForce = {(person.x - closestFood.x) * foodForcePower, (person.y - closestFood.y) * foodForcePower}
+        person.dx = -finalForce[1]
+        person.dy = -finalForce[2]
+    else
+        --do nothing
+    end
+
+
 end
 
 function randomMove(object)
+    -- random numbers: -1 to 1 scaled by speed
     object.dx = (rnd(2) - 1) * peopleSpeed
-    -- random number: -1 to 1 scaled by speed
     object.dy = (rnd(2) - 1) * peopleSpeed
-    -- random number: -1 to 1 scaled by speed
 end
