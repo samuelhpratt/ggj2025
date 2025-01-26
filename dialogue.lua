4debaf1ab2645308aaa7b7b8fc95206c32c85de1
Sunny <124411687+AlwaysMoths@users.noreply.github.com>
2025-01-26 13:44:02 +1100

    The people have spoken!

--- dialogue.lua
+++ dialogue.lua
@@ -1,156 +1,212 @@
 dialogueLines = {
     fire = {
         -- fanatic fire
-        "the creator's love, it\nshines bright like a fl-\nah, aghhhhh!",
-        "agh! great creator, i swear, \ni will not leave your\nembrace!",
-        "creator! why have you\nforsaken me!",
-        "i never believed, i never\nloved you creator! ahhhh!",
+        [12] = {
+            "the creator's love, it\nshines bright like a fl-\nah, aghhhhh!",
+            "agh! great creator, i swear, \ni will not leave your\nembrace!",
+            "creator! why have you\nforsaken me!",
+            "i never believed, i never\nloved you creator! ahhhh!"
+        },
         -- gigachad fire
+        [8] = {
         "not cool, brah! rule one of\ngiga-mikkel, no fir- ahhh!",
         "jokes on you creator, i'll\njust come back stronger!",
         "maximum overdrive! no pain\nno gain!",
-        "i would have made a better\ncreator than you brah!",
+        "i would have made a better\ncreator than you brah!"
+        },
         -- influencer fire
+        [11] = {
         "my perfect mikkel form! who\nwill guide the others?",
         "i wanted to stand out, not\nlike this! not like this!",
         "this mikkel is on\nfireeeeeeeeee!",
-        "just need this angle, yes,\ncloser is perfec- wait, agh!",
+        "just need this angle, yes,\ncloser is perfec- wait, agh!"
+        },
         -- babygirl fire
+        [10] = {
         "t^t",
         "why though?",
         "this is definitely not a\nvibe!",
         "i thought what we had was\nreal!"
+        }
     },
     fireSpace = {
         -- fanatic firespace
+        [12] = {
         "flames! summoned by the\ncreator, to punish mikkel!",
         "a gift from the creator!\nblessed be the dome!",
         "this is all my fault...\ni thought about leaving the\ndome!",
-        "we must complete a rain\nritual! oh creator, i\nbeseech thee!",
+        "we must complete a rain\nritual! oh creator, i\nbeseech thee!"
+        },
         -- gigachad firespace
+        [8] = {
         "i like a steamy room, but\nthis is too far brah!\nnot giga!",
         "double-dome dare you to\nsinge off mikkel's\neyebrushers.",
         "at iron-mikkel i ran through\n7 domes worth of fire.",
-        "brah this is your best prank\nyet! let me in on the next\none!",
-        -- influence firespace
+        "brah this is your best prank\nyet! let me in on the next\none!"
+        },
+        -- influencer firespace
+        [11] = {
         "oh my god, these domegram\nselfies are gonna be fire!",
         "top 10 ways to beg for mercy:\nnumber 6 will shock you!",
         "a major tragedy! this is\nperfect for relatability!",
-        "mikkel is never going to\nbelieve this!",
+        "mikkel is never going to\nbelieve this!"
+        },
         -- babygirl firespace
+        [10] = {
         "nothing like a good\nflame-roasted kib~bie!",
         "toasty warm, burnt toes, i'm\nfeelin' snug creator~boo!",
         "scary story time! i'll\nstart! once, there was a\ndome...",
         "mikkel smelled yum~my when\nthe fire bit them."
+        }
     },
     water = {
         -- fanatic wet
+        [12] = {
         "soothing water, drifting on\na dream, i wish to be free.",
         "i am unworthy of a\nreflection. creator, what do\ni look like?",
         "mikkel always ignores me.\nwhy? i'm hip, i'm cool,\ni'm pious.",
-        "from this water i shall\ndrink and grow in your image.",
+        "from this water i shall\ndrink and grow in your image."
+        },
         -- gigachad wet
+        [8] = {
         "water can't hurt beautiful\nmikkels.",
         "this water makes my muscles\ntotally shine, right creator?",
         "how does water even get in?\nyo, creator! will you spot\nme?",
-        "brah, i saw mikkel drinking\nfrom a puddle, suss right?",
+        "brah, i saw mikkel drinking\nfrom a puddle, suss right?"
+        },
         -- influencer wet
+        [11] = {
         "4 reasons why you should\nstop drinking water today.",
         "mikkel is throwing a pool\nparty. did you rsvp?",
         "damp kibbies is totally\ntrending right now.",
-        "i killed a man... ama!",
+        "i killed a man... ama!"
+        },
         -- babygirl wet
+        [10] = {
         "soggy kibbies are the best,\ndelicious mushy good~ness!",
         "if mikkels have no\nreflections, are we vampires?",
         "splish, splash, spl~osh.\nhey, mikkel, stop splashing\nme!",
         "pool party! let's get\nfloaties next, okay\ncreator~boo?"
+        }
     },
     foodSpace = {
         -- fanatic foodSpace
+        [12] = {
         "delicious cosmic treats,\nstars from the creator.",
         "a kibbie a day, keeps the\neternal damnation away!",
         "mikkel spat out their kibbie.\nyou should punish them,\ncreator.",
-        "they say kibbie is made from\nthe ashes of dissenters.",
-        -- gigachad foodSpace
+        "they say kibbie is made from\nthe ashes of dissenters."
+        },
+        [8] = {
+            -- gigachad foodSpace
         "thanks for getting me on\nthis all kibbie diet,\ncreator.",
         "i'm in my bulking phase, so\ni need double the kibbie!",
         "i'd kill for sugary kibbie.\nspecifically, mikkel.\ni hate that dude.",
-        "kibbie is just fuel for me\nto smash my goals!",
+        "kibbie is just fuel for me\nto smash my goals!"
+        },
         -- influencer foodSpace
+        [11] = {
         "i ate kibbie every day from\nbirth: a retrospective.",
         "mikkel creeps me out, they\nkeep scream-quoting my\ncontent at me.",
         "blend every kibbie together,\nsave time and hate kibbie!",
-        "be nice to the creator or\nthey'll spit in your kibbie!",
+        "be nice to the creator or\nthey'll spit in your kibbie!"
+        },
         -- babygirl foodSpace
+        [10] = {
         "hoard kibbies for a bad day.\neveryone likes a little\ntreat!",
         "my favourite flavour of\nkibbies is purple.",
         "one time i bit mikkel\nand they tasted like soggy\nkibbies. y~um.",
         "split kibbie with your lover.\nhalf the food, twice the\nresentment."
+        }
     },
     happy = {
         -- fanatic happy
+        [12] = {
         "it is blessed to be within\nthe dome, such a happy day!",
         "everyday i thank the creator\ni have no free will!",
         "do you hear us creator? we\nsing our thanks for this day!",
-        "today is so beautiful, i\nalmost forgot i am trapped\nin a dome.",
+        "today is so beautiful, i\nalmost forgot i am trapped\nin a dome."
+        },
         -- gigachad happy
+        [8] = {
         "i'm totally gonna smash my\npr today!",
         "everyday is leg day when\nyou're a mikkel!",
         "brah, why do they say break\na leg? that's like... half of\nmy being.",
-        "all this happiness is great\nfor the legs, i'm practically\njumping!",
+        "all this happiness is great\nfor the legs, i'm practically\njumping!"
+        },
         -- influencer happy
+        [11] = {
         "today is better than when i\ndoubled my organic\nvisibility!",
         "nothing like that stale dome\nair, what a day to be alive!",
         "oh my dome! i was having\nsuch a good time i nearly\nforgot to post!",
-        "hashtag happy hashtag\ndomelife hashtag creebies\nhashtag bestdayever!",
+        "hashtag happy hashtag\ndomelife hashtag creebies\nhashtag bestdayever!"
+        },
         -- babygirl happy
+        [10] = {
         "it's a g~ood day to be\ntrapped in a dome!",
         "you know what would make\nthis day better? ki~bbies!",
         "you, me, a packet of kibbies,\nthat mikkel over there.\nperfect, right?",
         "i'm motivated to get my\nsteps in! ... one! i did it!\nki~bbie time!"
+        }
     },
     unhappy = {
         -- fanatic unhappy
+        [12] = {
         "let me out of here! help,\nsomeone! oh... heh hello\ncreator!",
         "what more do you want from\nme?!",
         "i'm converting to cubism.",
-        "if i can't trust you creator,\nwhat's left?",
+        "if i can't trust you creator,\nwhat's left?"
+        },
         -- gigachad unhappy
+        [8] = {
         "i can't lift that's how down\nyou've got me, brah.",
         "it's hard to bench when\nyou've got the whole dome on\nyour shoulders.",
         "this dome giga-bytes.",
-        "it's my cheat day brah!\ndon't look at me! stop\nstaring at me!",
+        "it's my cheat day brah!\ndon't look at me! stop\nstaring at me!"
+        },
         -- influencer unhappy
+        [11] = {
         "top 10 places you don't want\nto live, starting with:\nthe dome!",
         "it's 1 star from me, and\nonly because 0 isn't an\noption!",
         "the dome is like hell, but\nwith marketing. that can only be\nbad.",
-        "i do not wish to be\nperceived right now.",
+        "i do not wish to be\nperceived right now."
+        },
         -- babygirl unhappy
+        [10] = {
         "it's just bad energy all\nround, the vibes are off.",
         "this is not a very demure,\ncutesy or mindful day you're\ngiving us!",
         "you've got that rank-a-stank\nenergy, creator~boo!",
         "i'm done, consider me\ndeceased, i quit!"
+        }
     },
     generic = {
         -- fanatic generic
+        [12] = {
         "the dome is forever, why\nwould mikkel want to leave?",
         "i was born to live and die\nin the dome. right, creator?",
         "the dome is my fate, we are\nintertwined. no surprises.",
-        "thank you, oh great creator\nfor this mighty dome!",
+        "thank you, oh great creator\nfor this mighty dome!"
+        },
         -- gigachad generic
+        [8] = {
         "sup creator, we're making\nmad progress on this dome\nproject.",
         "mikkel is a total\nbeta-mikkel, not like us,\nright creator?",
         "i could totally bench the\ndome, creator. what's your\npr?",
-        "creator, get in on the\nground floor on domecoin,\ntrust me.",
+        "creator, get in on the\nground floor on domecoin,\ntrust me."
+        },
         -- influencer generic
+        [11] = {
         "top 5 most domagramable\nlocations in the dome!",
         "i lived in a dome, you'll\nnever guess what happened\nnext!",
         "i'm hoping to get mikkel\nto manage my socials.",
-        "i'm talking a lot, but i'm\nnot really saying anything!",
+        "i'm talking a lot, but i'm\nnot really saying anything!"
+        },
         -- babygirl generic
+        [10] = {
         "mikkel and i are to~tal\nbesties, you're my bestest\nthough.",
         "doughhhhh~mah. doe~me!\ndommy. doh~meymey. heh.",
         "what's your dome like,\ncrea~tor? i bet it's super\nsparkly.",
         "let's make a secret\nhandshake. the secret? i have\nno hands!"
+        }
     }
 }
